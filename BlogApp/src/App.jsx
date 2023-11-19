cc3ea1606b4928aad124c37a3b14cfdf05495680
Sagar <04017711721_iot@vvips.edu>
2023-11-19 19:06:45 +0530

    sample of env

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 function App() {
 
+  console.log(import.meta.env.  VITE_MY_VARIABLE);
   return (
     <>
     <h2>Blog With App Write</h2>
---
+++ BlogApp/src/config/config.js
@@ -0,0 +1,7 @@
+// Configuration of the env variables
+
+const config = {
+    appwrite : String(import.meta)
+}
+
+export default config
